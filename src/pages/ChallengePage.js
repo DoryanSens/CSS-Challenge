af092c9a8851f106e7de3eed5bfcb049565a3bd8
DoryanSens <doryan.sens@supinfo.com>
2023-09-19 22:51:37 +0200

    add div challenge-zone

--- src/pages/ChallengePage.js
+++ src/pages/ChallengePage.js
@@ -10,18 +10,20 @@ const ChallengePage = ({ pageName }) => {
     const [id, setId] = useState(params.Id);
     const filePath = `./challenges/${id}/Challenge`;
 
-    useEffect(()=>{
+    useEffect(() => {
         navigate(`/Challenge/${id}`);
-    },[navigate, id])
+    }, [navigate, id]);
 
-    const updateIdChallengeFct = (newId) =>{
-        setId(newId)
-    }
+    const updateIdChallengeFct = (newId) => {
+        setId(newId);
+    };
 
     return (
         <>
             <Navbar pageName={pageName} updateIdChallengeFct={updateIdChallengeFct} />
-            <DynamicImport filePath={filePath} id={id} />
+            <div className="challenge-zone">
+                <DynamicImport filePath={filePath} id={id} />
+            </div>
         </>
     );
 };
